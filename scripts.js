8977d141e8db79a2af4b9c35228ea4d2a633c2bf
Mason Hamilton <Hamim949@hsd.k12.or.us>
2022-05-05 15:10:36 -0700

    broken

--- scripts.js
+++ scripts.js
@@ -1,5 +1,19 @@
-var paragraph = document.createElement('P');
+const paragraph = document.createElement('P');
 
-paragraph.textContent = 'the DOM is the bomb';
+paragraph.innerHTML  = "the DOM is the bomb";
 
 document.body.appendChild(paragraph);
+// new page
+var div = document.createElement('DIV');
+
+div.style.height = '100vh';
+
+document.body.appendChild(div);
+
+div.addEventListener('mousemove', function(event) {
+   console.log(event);
+   var x = event.clientX;
+   var y = event.clientY;
+   div.textContent = x + ', ' + y;
+   div.style.backroundcolor = 'rgb(' + x +', ' + y +',100)';
+});
